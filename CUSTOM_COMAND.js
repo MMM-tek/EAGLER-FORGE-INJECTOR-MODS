51ef7456e27c93af0ae92e71dc3f0f7eb72dcdd3
MMM-tek <mtmirallesflores@gmail.com>
2025-11-23 15:57:20 +0100

    Fix command handling and require player module

--- CUSTOM_COMAND.js
+++ CUSTOM_COMAND.js
@@ -2,10 +2,10 @@
 ModAPI.meta.title(CUSTOM COMMAND");
 ModAPI.meta.credits("MMM-tek");
 ModAPI.meta.description("");
+ModAPI.require('player');
 ModAPI.dedicatedServer.appendCode(function () {
     ModAPI.addEventListener("processcommand", (event) => {
         if (event.command.toLowerCase().startsWith("/test")) {
-            ModAPI.require('player');
                 if ('hi!'.charAt(0) !== "/") {
                     ModAPI.player.sendChatMessage({
                     message: 'hi!'
