f04b8a9ce6ad98b36e216cc94d75e8aad70bc7ef
MMM-tek <mtmirallesflores@gmail.com>
2025-11-23 17:01:53 +0100

    Update and rename Custom_Item.js to Hack_Item.js

--- Hack_Item.js
+++ Hack_Item.js
@@ -1,21 +1,21 @@
-(function CustomItemMod() {
+(function hackitemMod() {
     const itemTexture = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAKZJREFUOE9j/P//PxMDBIBoEP6HREOl4PLIciA2AyPIgMcM//7KgvWSDJjBBpx9/+YvJzc3Sbq12DhB6sEGsJ19/+YnmQawYhigzc7FcPXnN4KugbqAHWQAy9n3b34T4wJkw6EGYLqAoNVQBWS5ANlwZBfAvUCs/0EGkW0AzBKqGoCSDgh5A80F2KMRpAgfAKUT6kcjsfEPUycmKMQgy8AETkgUZWcAS3CPIf4oSPsAAAAASUVORK5CYII=";
 
-    ModAPI.meta.title("Custom Item Mod");
+    ModAPI.meta.title("Hack Item Mod");
     ModAPI.meta.icon(itemTexture);
-    ModAPI.meta.description("it's a custom item. what more do you want");
-    ModAPI.meta.credits("By <author_name>");
+    ModAPI.meta.description("To craft Bedrock and more...");
+    ModAPI.meta.credits("MMM-tek");
 
-    function CustomItem() {
+    function hackitem() {
         var creativeMiscTab = ModAPI.reflect.getClassById("net.minecraft.creativetab.CreativeTabs").staticVariables.tabMisc; //chuck it in the miscellaneous category ig
         var itemClass = ModAPI.reflect.getClassById("net.minecraft.item.Item"); //Get the item class
         var itemSuper = ModAPI.reflect.getSuper(itemClass, (x) => x.length === 1); //Get the super() function of the item class that has a length of 1
-        function CustomItem() {
+        function hackitem() {
             itemSuper(this); //Use super function to get block properties on this class.
             this.$setCreativeTab(creativeMiscTab); //Set the creative tab of the item to be the misc tab
         }
-        ModAPI.reflect.prototypeStack(itemClass, CustomItem); // ModAPI equivalent of `extends` in java
-        CustomItem.prototype.$onItemRightClick = function ($itemstack, $world, $player) { //example of how to override a method
+        ModAPI.reflect.prototypeStack(itemClass, hackitem); // ModAPI equivalent of `extends` in java
+        hackitem.prototype.$onItemRightClick = function ($itemstack, $world, $player) { //example of how to override a method
             //use ModAPI.util.wrap to create a proxy of the player and the world without $ prefixes on the properties and methods
             var player = ModAPI.util.wrap($player);
             var world = ModAPI.util.wrap($world);
@@ -33,18 +33,18 @@
 
         // Internal registration function. This will be used to actually register the item on both the client and the server.
         function internal_reg() {
-            // Construct an instance of the CustomItem, and set it's unlocalized name (translation id)
-            var custom_item = (new CustomItem()).$setUnlocalizedName(
-                ModAPI.util.str("custom_item")
+            // Construct an instance of the hackitem, and set it's unlocalized name (translation id)
+            var hack = (new hackitem()).$setUnlocalizedName(
+                ModAPI.util.str("hack")
             );
             //Register it using ModAPI.keygen() to get the item id.
-            itemClass.staticMethods.registerItem.method(ModAPI.keygen.item("custom_item"), ModAPI.util.str("custom_item"), custom_item);
+            itemClass.staticMethods.registerItem.method(ModAPI.keygen.item("hack"), ModAPI.util.str("hack"), hack);
 
             //Expose it to ModAPI
-            ModAPI.items["custom_item"] = custom_item;
+            ModAPI.items["hack"] = hack;
             
             //return the instance.
-            return custom_item;
+            return hack;
         }
 
         //if the item global exists (and it will on the client), register the item and return the registered instance.
@@ -57,21 +57,21 @@
     }
 
     // Run the function when the dedicated server loads.
-    ModAPI.dedicatedServer.appendCode(CustomItem); 
+    ModAPI.dedicatedServer.appendCode(hackitem); 
 
     // Run the function on the client
-    var custom_item = CustomItem();
+    var hack = hackitem();
 
     ModAPI.addEventListener("lib:asyncsink", async () => {
         ModAPI.addEventListener("lib:asyncsink:registeritems", (renderItem)=>{
-            renderItem.registerItem(custom_item, ModAPI.util.str("custom_item"));
+            renderItem.registerItem(hack, ModAPI.util.str("hack"));
         });
-        AsyncSink.L10N.set("item.custom_item.name", "Cool Custom Item");
-        AsyncSink.setFile("resourcepacks/AsyncSinkLib/assets/minecraft/models/item/custom_item.json", JSON.stringify(
+        AsyncSink.L10N.set("item.hack.name", "Cool Hack Item");
+        AsyncSink.setFile("resourcepacks/AsyncSinkLib/assets/minecraft/models/item/hack.json", JSON.stringify(
             {
                 "parent": "builtin/generated",
                 "textures": {
-                    "layer0": "items/custom_item"
+                    "layer0": "items/hack"
                 },
                 "display": {
                     "thirdperson": {
@@ -87,7 +87,7 @@
                 }
             }
         ));
-        AsyncSink.setFile("resourcepacks/AsyncSinkLib/assets/minecraft/textures/items/custom_item.png", await (await fetch(
+        AsyncSink.setFile("resourcepacks/AsyncSinkLib/assets/minecraft/textures/items/hack.png", await (await fetch(
             itemTexture
         )).arrayBuffer());
     });
